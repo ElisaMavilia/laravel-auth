0e90c12b67c62edbba66880b1cb895983f06e436
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-30 16:06:40 +0200

    added index.blade

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+
+@section('content')
+<main class="container mt-4">
+    <h1 class="pt-5 pb-3">Projects</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Slug</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                    <tr class="table table-dark table-striped-columns">
+                        
+                        <td>{{$project->id}}</td>
+                        <td>{{$project->title}}</td>
+                        <td>{{$project->content}}</td>
+                        <td scope="row">{{$project->slug}}</td>
+                    </tr>
+                </tbody>
+            @endforeach
+    </table>
+    </div>
+</main>
+
+@endsection
