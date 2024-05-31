64faaa2803155fa2c6b571d287d07d3986bcff6c
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-31 17:43:24 +0200

    added actions with route to the table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -11,6 +11,8 @@
                 <th scope="col">Title</th>
                 <th scope="col">Content</th>
                 <th scope="col">Slug</th>
+                <th scope="col">Update At</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -21,11 +23,22 @@
                         <td>{{$project->title}}</td>
                         <td>{{$project->content}}</td>
                         <td scope="row">{{$project->slug}}</td>
-                    </tr>
-                </tbody>
+                        <td>{{$project->created_at}}</td>
+                        <td>{{$project->updated_at}}</td>
+                        <td>
+                    <a href="{{route('admin.projects.show', $project->slug)}}"><i class="fa-solid fa-eye"></i></a>
+                    <a href="{{route('admin.projects.edit', $project->slug)}}"><i class="fa-solid fa-pen"></i></a>
+                    <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST" class="d-inline-block">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit" class="delete-button border-0 bg-transparent"  data-item-title="{{ $project->title }}">
+                        <i class="fa-solid fa-trash"></i>
+                      </button>
+                    </form>
+                </td>
+              </tr>
             @endforeach
-    </table>
-    </div>
-</main>
-
+          </tbody>
+      </table>
+</section>
 @endsection
